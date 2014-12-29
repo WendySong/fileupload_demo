fd35093033a2b3a1af840619f16950499b2bcd10
gaoke <gaoke@news.cn>
2014-12-29 18:08:38 +0800

    配置springmvc

---
+++ src/main/java/com/pisx/fileupload/controller/FileUploadController.java
@@ -0,0 +1,11 @@
+package com.pisx.fileupload.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by pisx on 2014/12/29.
+ */
+@Controller
+public class FileUploadController {
+    
+}
